9eb994f0e2d4dd1e42491c0c94173faf58bb96b7
abhikonkal <abhikonkal@gmail.com>
2023-12-08 08:55:00 +0530

    repo re named in github and demo-consense-mecahnisms are tried for implementing the hybrid POS_POA mechanism

---
+++ demo-consues-mech/c4.py
@@ -0,0 +1,190 @@
+import socket
+import sys
+import threading
+import re
+#from blockchain import Block, Blockchain
+import json
+
+#code for digital signature 
+
+import base64
+from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives.asymmetric import rsa
+from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.primitives.asymmetric import padding
+from cryptography.hazmat.primitives.serialization import Encoding, PrivateFormat, NoEncryption
+from cryptography.hazmat.primitives import serialization
+from cryptography.hazmat.primitives.serialization import Encoding
+import cryptography
+
+def rsa_keypair():
+    private_key = rsa.generate_private_key(public_exponent=65537, key_size=2048, backend=default_backend())
+    return (private_key, private_key.public_key())
+
+def sign(message, private_key):
+    padding_instance = padding.PSS(mgf=padding.MGF1(hashes.SHA256()), salt_length=padding.PSS.MAX_LENGTH)
+    return base64.b64encode(private_key.sign(message, padding_instance, hashes.SHA256()))
+
+def verify(message, signature, public_key):
+    sig = base64.b64decode(signature)
+    padding_instance = padding.PSS(mgf=padding.MGF1(hashes.SHA256()), salt_length=padding.PSS.MAX_LENGTH)
+    try:
+        public_key.verify(sig, message, padding_instance, hashes.SHA256())
+        return True
+    except cryptography.exceptions.InvalidSignature:
+        return False
+
+
+def deserialize_public_key(pub_key_string):
+    pub_key_bytes = pub_key_string.encode('utf-8')
+    pub_key = serialization.load_pem_public_key(pub_key_bytes, backend=default_backend())
+    return pub_key
+
+#digital signature code ends 
+
+# Add stake to client
+class Client:
+    def __init__(self):
+        self.stake = 1
+
+# Add total_weight and verify_transaction to Blockchain
+class Blockchain:
+    def __init__(self):
+        self.transactions = []
+        self.total_weight = 0
+
+    def verify_transaction(self, transaction, client):
+        # verify the transaction here
+        self.total_weight += client.stake
+        if self.total_weight > 78:
+            self.transactions.append(transaction)
+            self.total_weight = 0
+
+# Add Transaction class
+class Transaction:
+    def __init__(self, sender, receiver, amount, signature):
+        self.sender = sender
+        self.receiver = receiver
+        self.amount = amount
+        self.signature = signature
+
+ipofhost= '192.168.111.1'
+rendezvous = (ipofhost, 55555)
+
+msgbox=[]
+
+# connect to rendezvous
+print('connecting to rendezvous server')
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+sock.bind(('0.0.0.0', 50005))
+print('sending')
+sock.sendto(b'0', rendezvous)
+print('sent')
+
+neighbors = []
+
+while True:
+    data = sock.recv(1024).decode()
+    if data.strip() == 'ready':
+        print('checked in with server, waiting')
+        break
+
+data = sock.recv(1024).decode()
+print(data)
+ip1, sport1, dport1, ip2, sport2, dport2 = data.split(' ')
+sport1 = int(sport1)
+dport1 = int(dport1)
+sport2 = int(sport2)
+dport2 = int(dport2)
+
+print('\ngot peers')
+print('  ip1:          {}'.format(ip1))
+print('  source port1: {}'.format(sport1))
+print('  dest port1:   {}'.format(dport1))
+print('  ip2:          {}'.format(ip2))
+print('  source port2: {}'.format(sport2))
+print('  dest port2:   {}'.format(dport2))
+
+# connect to neighbors
+if ip1 != '':
+    print('connecting to neighbor 1: {}:{}'.format(ip1, dport1))
+    sock1 = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    sock1.connect((ip1, dport1))
+    neighbors.append(sock1)
+if ip2 != '':
+    print('connecting to neighbor 2: {}:{}'.format(ip2, dport2))
+    sock2 = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    sock2.connect((ip2, dport2))
+    neighbors.append(sock2)
+
+print('ready to exchange messages\n')
+
+#creating pvt,pub pairs 
+pvt1, pub1 = rsa_keypair()
+
+
+# listen for incoming messages
+def listen():
+    while True:
+        datajson = sock.recv(1024)
+        datajson = datajson.decode()
+        fetched_data=json.loads(datajson)
+        fetch_msg=fetched_data['raw_message']
+        topass_key=fetched_data['publickey']
+        sign=fetched_data['sign']
+        normal_msg = re.sub(r'^c\d+c\d+', '', fetch_msg)
+        data=normal_msg
+        ver_msg=fetch_msg.encode()
+        ver_sign=sign.encode()
+        ver_pub=deserialize_public_key(topass_key)
+        if(verify(ver_msg,ver_sign,ver_pub)):
+            print('Message Verification Succefull')
+            if normal_msg in msgbox:
+                print('dup found')
+            else:
+                msgbox.append(normal_msg)
+                print('\rneighbor: {}\n> '.format(data), end='')
+                pattern = r"^c\d+c\d+.*$"
+                reg_match=re.match(pattern, fetch_msg)
+                if not reg_match:
+                    for neighbor in neighbors:
+                        msg=data
+                        datajson={
+                            'raw_message':msg,
+                            'sign':sign,
+                            'publickey':topass_key
+                        }
+                        datajson=json.dumps(datajson)
+                        neighbor.send(datajson.encode())
+                        # print(datajson)
+                        print('broadcasted')
+        else:
+            print('Message Verification Failed!!.Message is Tampered')
+                
+                
+listener = threading.Thread(target=listen, daemon=True)
+listener.start()
+
+
+# send messages to neighbors
+pub_key_bytes = pub1.public_bytes(Encoding.PEM, serialization.PublicFormat.SubjectPublicKeyInfo)
+# Convert bytes to string (optional)
+pub_key_string = pub_key_bytes.decode('utf-8')
+while True:
+    msg = input('> ')
+    msg = 'c1'+msg
+    msgbox.append(msg)
+    msg=msg.encode()
+    signmsg = sign(msg, pvt1)
+    signmsg=signmsg.decode()
+    msg=msg.decode()
+    datajson={
+        'raw_message':msg,
+        'sign':signmsg,
+        'publickey':pub_key_string
+    }
+    datajson=json.dumps(datajson)
+    for neighbor in neighbors:
+        neighbor.send(datajson.encode())
+        print('sent')
---
+++ demo-consues-mech/server.py
@@ -0,0 +1,38 @@
+import socket
+
+known_port = 50001
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+sock.bind(('0.0.0.0', 55555))
+
+while True:
+    clients = []
+
+    while len(clients) < 4:
+        data, address = sock.recvfrom(128)
+
+        print('connection from: {}'.format(address))
+        clients.append(address)
+
+        sock.sendto(b'ready', address)
+
+    c1 = clients[0]
+    c2 = clients[1]
+    c3 = clients[2]
+    c4 = clients[3]
+
+    c1_addr, c1_port = clients[0]
+    c2_addr, c2_port = clients[1]
+    c3_addr, c3_port = clients[2]
+    c4_addr, c4_port = clients[3]
+
+    print(c1_addr, c1_port, known_port)
+    print(c2_addr, c2_port, known_port)
+    print(c3_addr, c3_port, known_port)
+    print(c4_addr, c4_port, known_port)
+
+    # send messages to neighbors
+    sock.sendto('{} {} {} {} {} {}'.format(c2_addr, c1_port, c2_port,c4_addr, c1_port, c4_port).encode(), c1)
+    sock.sendto('{} {} {} {} {} {}'.format(c3_addr, c2_port, c3_port,c1_addr, c2_port, c1_port).encode(), c2)
+    sock.sendto('{} {} {} {} {} {}'.format(c4_addr, c3_port, c4_port,c2_addr, c3_port, c2_port).encode(), c3)
+    sock.sendto('{} {} {} {} {} {}'.format(c1_addr, c4_port, c3_port,c3_addr, c4_port, c1_port).encode(), c4)
