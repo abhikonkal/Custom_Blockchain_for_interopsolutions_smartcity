fe10fa0f51464fe606ad60e80126e96ac6e1648d
reverseabhi <abhikonkal@gmail.com>
2024-02-01 15:40:40 +0530

    adding ppt and pdf

--- p2p_rectangle/server.py
+++ p2p_rectangle/server.py
@@ -5,34 +5,42 @@ known_port = 50001
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 sock.bind(('0.0.0.0', 55555))
 
+normal_node_weight = 5
+authority_weight=61
+threshold=60
+
+
 while True:
-    clients = []
+    try:
+        clients = []
 
-    while len(clients) < 4:
-        data, address = sock.recvfrom(128)
+        while len(clients) < 4:
+            data, address = sock.recvfrom(128)
 
-        print('connection from: {}'.format(address))
-        clients.append(address)
+            print('connection from: {}'.format(address))
+            clients.append(address)
 
-        sock.sendto(b'ready', address)
+            sock.sendto(b'ready', address)
 
-    c1 = clients[0]
-    c2 = clients[1]
-    c3 = clients[2]
-    c4 = clients[3]
+        c1 = clients[0]
+        c2 = clients[1]
+        c3 = clients[2]
+        c4 = clients[3]
 
-    c1_addr, c1_port = clients[0]
-    c2_addr, c2_port = clients[1]
-    c3_addr, c3_port = clients[2]
-    c4_addr, c4_port = clients[3]
+        c1_addr, c1_port = clients[0]
+        c2_addr, c2_port = clients[1]
+        c3_addr, c3_port = clients[2]
+        c4_addr, c4_port = clients[3]
 
-    print(c1_addr, c1_port, known_port)
-    print(c2_addr, c2_port, known_port)
-    print(c3_addr, c3_port, known_port)
-    print(c4_addr, c4_port, known_port)
+        print(c1_addr, c1_port, known_port)
+        print(c2_addr, c2_port, known_port)
+        print(c3_addr, c3_port, known_port)
+        print(c4_addr, c4_port, known_port)
 
-    # send messages to neighbors
-    sock.sendto('{} {} {} {} {} {}'.format(c2_addr, c1_port, c2_port,c4_addr, c1_port, c4_port).encode(), c1)
-    sock.sendto('{} {} {} {} {} {}'.format(c3_addr, c2_port, c3_port,c1_addr, c2_port, c1_port).encode(), c2)
-    sock.sendto('{} {} {} {} {} {}'.format(c4_addr, c3_port, c4_port,c2_addr, c3_port, c2_port).encode(), c3)
-    sock.sendto('{} {} {} {} {} {}'.format(c1_addr, c4_port, c3_port,c3_addr, c4_port, c1_port).encode(), c4)
+        # send messages to neighbors
+        sock.sendto('{} {} {} {} {} {} {} {}'.format(c2_addr, c1_port, c2_port,c4_addr, c1_port, c4_port,normal_node_weight,threshold).encode(), c1)
+        sock.sendto('{} {} {} {} {} {} {} {}'.format(c3_addr, c2_port, c3_port,c1_addr, c2_port, c1_port,normal_node_weight,threshold).encode(), c2)
+        sock.sendto('{} {} {} {} {} {} {} {}'.format(c4_addr, c3_port, c4_port,c2_addr, c3_port, c2_port,normal_node_weight,threshold).encode(), c3)
+        sock.sendto('{} {} {} {} {} {} {} {}'.format(c1_addr, c4_port, c3_port,c3_addr, c4_port, c1_port,authority_weight,threshold).encode(), c4)
+    except KeyboardInterrupt:
+        break
